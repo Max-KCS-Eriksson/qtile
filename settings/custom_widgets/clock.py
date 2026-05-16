2237e3ad484ef2228ada91f89f48359ea4774cb9
Max Eriksson <teratoscincus@hotmail.com>
2026-05-16 17:50:28 +0200

    fix(clock): compensate time diff

--- settings/custom_widgets/clock.py
+++ settings/custom_widgets/clock.py
@@ -6,4 +6,4 @@ from libqtile.widget.clock import Clock
 class MyClock(Clock):
     """Compensating that libqtile.widget.clock.Clock showing incorrect time."""
 
-    DELTA = timedelta(minutes=10, seconds=0.5)
+    DELTA = timedelta(minutes=17, seconds=0.5)
